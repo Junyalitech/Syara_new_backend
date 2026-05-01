206b602d8de7449cfc80597b0a9bcaa1a63162ba
Kartikay786 <rohit2141451519@emil>
2026-05-02 02:00:57 +0530

    sms add

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,16 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_URL, {
-  dialect: 'mysql',
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    logging: false,
+  }
+);
 
 sequelize.authenticate()
   .then(() => {
@@ -20,6 +20,30 @@ sequelize.authenticate()
     console.error('❌ Unable to connect:', err);
   });
 
+module.exports = sequelize;
+
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(process.env.DB_URL, {
+//   dialect: 'mysql',
+//   logging: false,
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,
+//     },
+//   },
+// });
+
+// sequelize.authenticate()
+//   .then(() => {
+//     console.log('✅ MySQL connected successfully.');
+//   })
+//   .catch(err => {
+//     console.error('❌ Unable to connect:', err);
+//   });
+
 
   
-module.exports = sequelize;
+// module.exports = sequelize;
